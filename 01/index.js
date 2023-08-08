0ca2020add1772f43decfe802bd666af4441a38b
Amanda Santos <amanda280721@gmail.com>
2023-08-07 23:31:16 -0400

    Resolvido

---
+++ 01/index.js
@@ -0,0 +1,24 @@
+const endereco = {
+    rua: 'Boulevard',
+    numero: 2,
+    complemento: 'S/C',
+    bairro: 'Cidade Nova',
+    cep: '09090-909',
+    cidade: 'Manaus',
+    estado: 'AM',
+    pais: 'Brasil'
+}
+
+const usuario = {
+    nome: 'Amanda',
+    email: '123@gmail.com',
+    telefone: '(92)99999-9999',
+    data_nascimento: '05/09/1996',
+    endereco
+}
+
+console.log(usuario.endereco);
+
+console.log(usuario.nome);
+
+console.log(endereco.cidade)
